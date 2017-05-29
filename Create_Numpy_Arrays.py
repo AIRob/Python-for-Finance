9d9490b509805f99a1b43cc956ae86156e3b0757
Kenneth Adair <SOREASAN@GMAIL.COM>
2017-05-29 16:35:36 -0600

    Created a demo of creating a Numpy array with initial values.  Taken from lectures

---
+++ Create_Numpy_Arrays.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# Creating NumPy arrays.
+import numpy as np
+
+def test_run():
+    # 1d array
+    print np.array([2, 3, 4])
+    # List of tuples to 2d array
+    print np.array([(2, 3, 4), (5, 6, 7)])
+
+if __name__ == "__main__":
+    test_run()
