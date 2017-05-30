75c705330543ee4b78e28da52f9650d325a95815
Kenneth Adair <SOREASAN@GMAIL.COM>
2017-05-29 18:16:37 -0600

    Added a demo of timestamps in Python.  Taken from lectures.

---
+++ Time.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# Using time function.
+import time
+
+def test_run():
+    t1 = time.time()
+    print "Hello World!"
+    t2 = time.time()
+    print "The time taken by print statement is ", t2 - t1, " seconds"
+
+if __name__ == "__main__":
+    test_run()
