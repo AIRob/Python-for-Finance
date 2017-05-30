958a8afdca8b5d40d7b4672fc8563e8cdf211478
Kenneth Adair <SOREASAN@GMAIL.COM>
2017-05-29 18:39:45 -0600

    Added a demo of modifying array elements and indexing arrays using another array with a list of indexes.  Taken from lectures.

---
+++ Indexing_Array_With_Another_Array.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# Accessing elements
+
+import numpy as np
+
+def test_run():
+    a = np.random.rand(5)
+
+    #accessing using list of indices
+    indices = np.array([1, 1, 2, 3])
+    print a[indices]
+
+if __name__ == "__main__":
+    test_run()
+
